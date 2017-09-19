4190af3a789c986fd3658ee8eb3bb3c03f376bf8
Azrael <wuyanbin@sansi.com>
2017-09-19 17:19:31 +0800

    update log_util.js

--- utils/log_util.js
+++ utils/log_util.js
@@ -33,7 +33,7 @@ var formatRes = function (ctx, resTime) {
     // 响应状态码
     logText += 'response status: ' + ctx.status + '\n';
     // 响应内容
-    logText += 'response body: ' + JSON.stringify(ctx.body) + '\n';
+    logText += 'response body: ' + '\n' + JSON.stringify(ctx.body) + '\n';
     // 响应日志结束
     logText += "*************** response log end ***************" + "\n";
     return logText;
