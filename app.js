d32aeb6a24a9e91260a2ae3084bdd2ede5a18146
Azrael <wuyanbin@sansi.com>
2017-09-20 20:01:04 +0800

    update all packages version, add cors middleware, remove koa-views middleware

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
-const views = require('koa-views');
-const json = require('koa-json');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser');
-const koaStatic = require('koa-static');
-const jwt = require('jsonwebtoken');
-const koaJwt = require('koa-jwt');
+const json = require('koa-json');
+const static = require('koa-static');
+const jwt = require('koa-jwt');
+const cors = require('kcors');
 const logUtil = require('./utils/log_util');
 const jwtConfig = require('./config/jwt_config');
 const router = require('./routers/route');
@@ -15,21 +14,26 @@ onerror(app);
 
 // middlewares
 app.use(bodyparser({
-	enableTypes: ['json', 'form', 'text']
+	enableTypes: ['json', 'form', 'text']		// parser will only parse when request type hits enableTypes, default is ['json', 'form'].
 }));
+
+app.use(json({
+	pretty: true,		// default to pretty response, default to true
+	param: 'pretty',	// optional query-string param for pretty responses, default to none
+	spaces: 4			// JSON spaces, default to 2
+}));
+
+app.use(cors({
+	origin: 'http://localhost:3001/',							// {String|Function(ctx)} origin `Access-Control-Allow-Origin`, default is '*'
+	allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],		// {String|Array} allowMethods `Access-Control-Allow-Methods`, default is 'GET,HEAD,PUT,POST,DELETE,PATCH'
+}))
 
 // jwt
-app.use(koaJwt({secret: jwtConfig.secret}).unless({
-	path: [/^\/api\/login/]		// 数据中的路径不需要要通过jwt验证
+app.use(jwt({secret: jwtConfig.secret}).unless({
+	path: [/^\/api\/login/, /^\/api\/register/]		// 数据中的路径不需要要通过jwt验证
 }));
 
-app.use(json());
-
-app.use(koaStatic(__dirname + '/public'));
-
-app.use(views(__dirname + '/views', {
-	extension: 'pug'
-}));
+app.use(static(__dirname + '/public'));
 
 // logger
 app.use(async (ctx, next) => {
